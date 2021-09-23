23913bf73f69a738fa267f4a3afd77abd72544c5
Kie Kobayashi <Yijeonhyung@ijeonhyeongui-MacBook-Pro.local>
2021-09-23 15:48:35 +0900

    商品購入明細画面の実装#15

---
+++ www/html/order_detail.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../conf/const.php';
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'user.php';
+require_once MODEL_PATH . 'item.php';
+require_once MODEL_PATH . 'cart.php';
+require_once MODEL_PATH . 'order.php';
+
+session_start();
+
+if(is_logined() === false){
+  redirect_to(LOGIN_URL);
+}
+
+$db = get_db_connect();
+$user = get_login_user($db);
+
+$order_id = get_post('order_id');
+
+if(is_admin($user) === false){
+  $orders = get_order($db, $user['user_id'], $order_id);
+} else {
+  $orders = get_admin_order($db, $order_id);
+}
+
+$order_details = get_order_detail($db, $order_id);
+
+include_once VIEW_PATH . 'order_detail_view.php';
